660558ded532c947b2fc8f51dc27b5036d0f97f7
Dennis Tei-Muno <munodennis@gmail.com>
2025-11-28 08:16:32 -0600

    Create 1-splunk-time-search.sh

---
+++ 1-splunk-time-search.sh
@@ -0,0 +1,11 @@
+#search for absolute time
+"failed password" earliest=09/03/2025:00:00:00 latest=09/04/2025:00:00:00
+
+#search for relative time
+"failed password" earliest=-24@h latest=now
+
+#*
+time unit abbreviations
+s=seconds m=minutes h=hours d=days mon=months y=years
+*#
+
